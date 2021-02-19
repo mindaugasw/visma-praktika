92180694b31575fce536c3a1bd73e1882d542a66
Mindaugas <mindaugas932@gmail.com>
2021-02-19 13:48:28 +0200

    Additional QueryBuilder tests

--- tests/Service/DB/QueryBuilderTest.php
+++ tests/Service/DB/QueryBuilderTest.php
@@ -26,6 +26,47 @@ class QueryBuilderTest extends TestCase
         $this->assertSame($expectedSql, $sql);
     }
     
+    public function testJoin(): void
+    {
+        $sql = (new QueryBuilder())
+            ->select('word_to_pattern.position', 'hyphenation_pattern.*')
+            ->from('word_to_pattern')
+            ->joinOn('hyphenation_pattern', 'word_to_pattern.pattern_id=hyphenation_pattern.id')
+            ->where('word_to_pattern.word_id=?')
+            ->getQuery();
+        
+        $expectedSql =
+            'SELECT word_to_pattern.position, hyphenation_pattern.*  FROM word_to_pattern '
+            .' JOIN hyphenation_pattern ON word_to_pattern.pattern_id=hyphenation_pattern.id '
+            .' WHERE word_to_pattern.word_id=?';
+    
+        $sql = $this->removeExtraSpaces($sql);
+        $expectedSql = $this->removeExtraSpaces($expectedSql);
+    
+        $this->assertSame($expectedSql, $sql);
+    }
+    
+    public function testInsert(): void
+    {
+        $sql = (new QueryBuilder())
+            ->insertInto(
+                'hyphenation_pattern',
+                ['pattern', 'patternNoDot', 'patternNoNumbers', 'patternText', 'patternType']
+            )
+            ->values('?,?,?,?,?', 3)
+            ->getQuery();
+    
+        $expectedSql =
+             'INSERT INTO hyphenation_pattern '
+            .'(pattern, patternNoDot, patternNoNumbers, patternText, patternType) '
+            .' VALUES (?,?,?,?,?), (?,?,?,?,?), (?,?,?,?,?)';
+    
+        $sql = $this->removeExtraSpaces($sql);
+        $expectedSql = $this->removeExtraSpaces($expectedSql);
+    
+        $this->assertSame($expectedSql, $sql);
+    }
+    
     /**
      * All QueryBuilder methods add extra spaces around all statements.
      * Remove any extra spaces, leaving no more than 1 consecutive space
